ca3087ac48d845a363b340ac875d5b1c7420c4f3
felo <167656073+felossj@users.noreply.github.com>
2026-05-11 14:01:03 -0300

    Add files via upload

---
+++ t_estufa.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-05-2026 a las 20:08:00
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_estufa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `t_estufa`
+--
+
+CREATE TABLE `t_estufa` (
+  `id` int(11) NOT NULL,
+  `t_act` float NOT NULL,
+  `t_eleg` float NOT NULL,
+  `estado` varchar(2) NOT NULL,
+  `c_amb` varchar(2) NOT NULL,
+  `hora` time NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `t_estufa`
+--
+ALTER TABLE `t_estufa`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
